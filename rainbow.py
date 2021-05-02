1b2912bcc4c2f9077816b9f9a0a511f3e624b5a1
Ian Moreno <ian.moreno@parkmobile.io>
2021-05-02 15:59:08 -0400

    Trying to get rainbow coloring to work

---
+++ rainbow.py
@@ -0,0 +1,21 @@
+import board
+import neopixel
+
+
+num_lights = 50
+
+pixels = neopixel.NeoPixel(board.D18, num_lights)
+
+#I need all combinations of R,G,B and I need to iterate over all of them
+
+colors = []
+
+for r in range(255, -1, -1):
+	for g in range(255, -1, -1):
+		for b in range(255, -1, -1):
+			colors.append((r, g, b))
+
+for color in colors:
+	pixels.fill(color)
+	pixels.show()
+	sleep(0.1)
