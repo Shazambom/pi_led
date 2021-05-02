4d419daa82b3c1cf82cda5df3f9c44aa16cb2c8b
Ian Moreno <ian.moreno@parkmobile.io>
2021-05-02 17:04:21 -0400

    Removing debug prints

--- sin_rainbow.py
+++ sin_rainbow.py
@@ -15,9 +15,7 @@ pixels = neopixel.NeoPixel(board.D18, num_lights)
 
 def NextColor(color):
 	cin = np.true_divide(np.array(color), 255) * 360
-	print("Angle IN: " + str(cin))
 	rad = np.absolute(np.sin(cin * np.pi / 180.))
-	print("Radians out: " + str(rad))
 	
 	return (rad[0] * 255, rad[1] * 255, rad[2] * 255)
 
@@ -27,6 +25,5 @@ for i in range(0, 10000):
 	pixels.fill(color)
 	pixels.show()
 	color = NextColor(color)
-	print(color)
 	time.sleep(0.5)
 
