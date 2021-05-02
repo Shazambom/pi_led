013360adbed4291cd7a165324ce49d7ef74187ea
Ian Moreno <ian.moreno@parkmobile.io>
2021-05-02 15:35:59 -0400

    further tests, documentation on this thing isn't good

--- test.py
+++ test.py
@@ -7,6 +7,12 @@ pixels = neopixel.NeoPixel(board.D18, num_lights)
 
 pixels[19] = (0, 255, 0)
 
+pixels[0] = (255, 0, 0)
+
+pixels[1] = (0, 0, 255)
+
+print(pixels)
+
 pixels.fill((255, 0, 0))
 
 pixels.fill((0,0,0))
