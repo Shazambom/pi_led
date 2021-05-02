06197fc3ae85193e8998c8d5d2a5e3642c8db25e
Ian Moreno <ian.moreno@parkmobile.io>
2021-05-02 16:10:09 -0400

    Trying out sin waves

--- rainbow.py
+++ rainbow.py
@@ -1,6 +1,7 @@
 import board
 import neopixel
 import time
+import numpy as np
 
 
 num_lights = 50
@@ -8,15 +9,18 @@ num_lights = 50
 pixels = neopixel.NeoPixel(board.D18, num_lights)
 
 #I need all combinations of R,G,B and I need to iterate over all of them
+#I need colors from ROYGBIV but I want it to transition in between these colors seamlessly so 
+#I want a function I can imput the current value and it know how to step to the next color
 
-colors = []
 
-for r in range(255, -1, -1):
-	for g in range(255, -1, -1):
-		for b in range(255, -1, -1):
-			colors.append((r, g, b))
+def NextColor(color):
+	out = np.sin(np.array(color) * np.pi / 180.)
+	return (out[0], out[1], out[2])
 
-for color in colors:
+
+color = (255, 0, 100)
+while True:
 	pixels.fill(color)
 	pixels.show()
-	# sleep(0.1)
+	color = NextColor(color)
+
