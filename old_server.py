44bbbb6f3aefa1755b24c4d609ff8d1c21fbcc3e
Ian Moreno <ian.moreno@parkmobile.io>
2021-05-06 14:05:19 -0400

    Moving to flask for server management

---
+++ old_server.py
@@ -0,0 +1,44 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+import atexit
+# from led import Led
+from encoder import Encoder
+
+HOST = "0.0.0.0"
+PORT = 9000
+
+num_lights = 50
+num_colors = 250
+# controller = Led(num_lights, num_colors)
+codec = Encoder(num_lights, num_colors)
+
+class LEDServer(BaseHTTPRequestHandler):
+	def _set_response(self):
+		self.send_response(200)
+		self.send_header('Content-type', 'text/html')
+		self.end_headers()
+
+	def do_POST(self):
+		content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+		post_data = self.rfile.read(content_length) # <--- Gets the data itself
+
+		print(post_data)
+		# controller.decode(post_data)
+		self._set_response()
+
+def on_exit():
+	server.server_close()
+	print("Server stopped.")
+
+if __name__ == "__main__":
+	atexit.register(on_exit)
+
+	# controller.run()
+
+	server = HTTPServer((HOST, PORT), LEDServer)
+	print("Server started http://%s:%s" % (HOST, PORT))
+
+	server.serve_forever()
+
+
+	
--- server.py
+++ server.py
@@ -1,42 +1,18 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import time
-import atexit
-from led import Led
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import  FileStorage
+app = Flask(__name__, template_folder='./html')
 
-HOST = "0.0.0.0"
-PORT = 9000
-
-num_lights = 50
-num_colors = 250
-controller = Led(num_lights, num_colors)
-
-class LEDServer(BaseHTTPRequestHandler):
-	def _set_response(self):
-		self.send_response(200)
-		self.send_header('Content-type', 'text/html')
-		self.end_headers()
-
-	def do_POST(self):
-		content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-		post_data = self.rfile.read(content_length) # <--- Gets the data itself
-
-		print(post_data)
-		controller.decode(post_data)
-		self._set_response()
-
-def on_exit():
-	server.server_close()
-	print("Server stopped.")
-
-if __name__ == "__main__":
-	atexit.register(on_exit)
-
-	controller.run()
-
-	server = HTTPServer((HOST, PORT), LEDServer)
-	print("Server started http://%s:%s" % (HOST, PORT))
-
-	server.serve_forever()
-
-
+@app.route('/upload')
+def upload_file():
+   return render_template('upload.html')
 	
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file_route():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      return 'file uploaded successfully'
+		
+if __name__ == '__main__':
+   app.run(debug = True)
