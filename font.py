ec2d5f4a5f8e9f471b8f1a4dced475893eda1e8d
Ian Moreno <ian.moreno@parkmobile.io>
2021-05-10 18:12:19 -0400

    Updating todo

--- font.py
+++ font.py
@@ -14,7 +14,7 @@ class Font:
 		self.font = json.load(font_file)
 		font_file.close()
 
-#TODO: Still need to get the
+#TODO: Still need to get the begining of the frames to start with off leds so the text scrolls past properly
 	def TextToFrames(self, text):
 		if not text.isalnum():
 			return [[]]
@@ -67,15 +67,6 @@ images[0].save('font_experiment.gif', save_all=True, append_images=images[1:], o
 
 
 
-
-
-
-# 	<----------			<----------
-#  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,
-# 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-# 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-# 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
-# 40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
 
 
 # 	<----------			<----------
