397ee2c86507de6d962996153b3109d0a501f8b1
Ian Moreno <ian.moreno@parkmobile.io>
2021-05-15 18:02:12 -0400

    Adding decay

--- generator.py
+++ generator.py
@@ -286,7 +286,7 @@ class Generator:
 							avg_color[2] += n[2]
 						buff[x][y] = (int(avg_color[0] / 8), int(avg_color[1] / 8), int(avg_color[2] / 8))
 					else:
-						buff[x][y] = board[x][y]
+						buff[x][y] = (board[x][y][0] - 5, board[x][y][1] - 5, board[x][y][2] - 5)
 
 			frames.append(frame)
 			board = copy.deepcopy(buff)
