d5515a85183cdf2709c6f741a92d7b34e275a012
Ian Moreno <ian.moreno@parkmobile.io>
2021-05-15 12:48:18 -0400

    changing itteration starting point

--- generator.py
+++ generator.py
@@ -129,9 +129,9 @@ class Generator:
 			board[pos] = color
 			frames.append(copy.deepcopy(board))
 			
-			for x in range(self.width - 2, -1, -1):
+			for x in range(self.width - 1, -1, -1):
 				if pos - self.height < 0 or board[self.lookup[x][y]] != off:
-					itter = board[1:pos-1]
+					itter = board[1:pos]
 					color = self.next_color_rainbow(color)
 					itter.append(color)
 					board[0:pos] = itter
